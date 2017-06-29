e4ba0bb628b077cc0d6e142d4ef9206fbb9850b6
Dan <=>
2017-06-29 23:39:45 +0100

    extract ItemEffect to own file

---
+++ src/SrpTask/Game/ItemEffect.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SrpTask.Game
+{
+    public interface ItemEffect
+    {
+        void Effect(Item item, RpgPlayer rpgPlayer, IGameEngine gameEngine, Action<Item, RpgPlayer> inventoryCallback);
+    }
+}
--- src/SrpTask/Game/RegularItemEffect.cs
+++ src/SrpTask/Game/RegularItemEffect.cs
@@ -2,11 +2,6 @@ using System;
 
 namespace SrpTask.Game
 {
-    public interface ItemEffect
-    {
-        void Effect(Item item, RpgPlayer rpgPlayer, IGameEngine gameEngine, Action<Item, RpgPlayer> inventoryCallback);
-    }
-
     public class RegularItemEffect : ItemEffect
     {
         public void Effect(Item item, RpgPlayer rpgPlayer, IGameEngine gameEngine, Action<Item, RpgPlayer> inventoryCallback)
